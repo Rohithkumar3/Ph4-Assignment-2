7d72be7e3637ec2f83ad448a428aa78738f43a99
Rohithkumar <rohithkumargande@gmail.com>
2024-01-08 16:42:02 +0530

    first commit

---
+++ Assignment2/UserAuthenticator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class UserAuthenticator
+    {
+        private List<User> users = new List<User>();
+        public bool RegisterUser(string username, string password)
+        {
+            if (users.Any(u => u.Username == username))
+            {
+                return false; // User already exists
+            }
+
+            users.Add(new User(username, password));
+            return true; // User registered successfully
+        }
+
+        public bool LoginUser(string username, string password)
+        {
+            return users.Any(u => u.Username == username && u.Password == password);
+        }     
+    }
+}
---
+++ Assignment2/UserAuthenticatorTest.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    [TestFixture]
+    public class UserAuthenticatorTests
+    {
+        private UserAuthenticator userAuthenticator;
+
+        [SetUp]
+        public void Setup()
+        {
+            userAuthenticator = new UserAuthenticator();
+        }
+
+        [Test]
+        public void TestUserRegistration()
+        {
+            string username = "Rohith";
+            string password = "password123";
+
+            bool registrationResult = userAuthenticator.RegisterUser(username, password);
+            Assert.IsTrue(registrationResult);
+        }
+        [Test]
+        public void RegisterUser_FailedRegistration_DuplicateUser()
+        {
+            userAuthenticator.RegisterUser("existinguser", "password");
+
+            bool result = userAuthenticator.RegisterUser("existinguser", "newpassword");
+            Assert.IsFalse(result);
+        }
+
+
+        [Test]
+        public void TestUserLogin_ValidCredentials_ReturnsTrue()
+        {
+            string username = "Rohith";
+            string password = "password123";
+
+            userAuthenticator.RegisterUser(username, password);
+
+            bool loginResult = userAuthenticator.LoginUser(username, password);
+
+            Assert.IsTrue(loginResult);
+        }
+
+        [Test]
+        public void TestUserLogin_InvalidCredentials_ReturnsFalse()
+        {
+            string username = "Rohith";
+            string password = "password123";
+
+            userAuthenticator.RegisterUser(username, password);
+
+            bool loginResult = userAuthenticator.LoginUser(username, "wrongpassword");
+            Assert.IsFalse(loginResult);
+        }
+    }
+}
